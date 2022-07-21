e510af4e26e40085872f2af55a89e11dbf2f76a5
sukhadia_s <ssukhadia711@gmail.com>
2022-07-21 07:55:53 -0400

    transposing dataframe and setting headers

--- Imagene_v2_2.py
+++ Imagene_v2_2.py
@@ -462,7 +462,9 @@ def BuildModel(train , Y_train , test , Y_test , method, params, cv_par, scoring
                 ##Get an array of "True or False" for features. True means selected, False means not-selected. Selection happens through feature importances yield by the model using SelectFromModel function above.
                 feature_selected_or_not_=selector.get_support()
                 ##Make a dataframe of that array which has the header as names of each feature.
-                fsd=pd.DataFrame(data=feature_selected_or_not_,columns=column_headers__)
+                fsd=pd.DataFrame(data=feature_selected_or_not_)
+                fsd=pd.DataFrame.transpose(fsd)
+                fsd.columns=column_headers__
                 print(fsd)
                 ##Drop the features that are "False", i.e. not selected.
                 fsd=fsd.drop(columns=fsd.columns[(fsd == 'False').any()])
